5ac5d992b99386be009a1ffa4051c2b37b9fab09
Viswesh-N <nvichu1001@gmail.com>
2025-02-05 01:41:13 -0800

    added box decomposition

--- mani_skill/utils/building/actors/ycb.py
+++ mani_skill/utils/building/actors/ycb.py
@@ -4,38 +4,43 @@ from mani_skill.utils.io_utils import load_json
 
 YCB_DATASET = dict()
 
-
 def _load_ycb_dataset():
     global YCB_DATASET
     YCB_DATASET = {
         "model_data": load_json(ASSET_DIR / "assets/mani_skill2_ycb/info_raw.json"),
     }
 
-
 def get_ycb_builder(
     scene: ManiSkillScene, id: str, add_collision: bool = True, add_visual: bool = True
 ):
-    if "YCB" not in YCB_DATASET:
+    if "model_data" not in YCB_DATASET:
         _load_ycb_dataset()
     model_db = YCB_DATASET["model_data"]
 
     builder = scene.create_actor_builder()
-
     metadata = model_db[id]
     density = metadata.get("density", 1000)
     model_scales = metadata.get("scales", [1.0])
     scale = model_scales[0]
     physical_material = None
-    (metadata["bbox"]["max"][2] - metadata["bbox"]["min"][2]) * scale
     model_dir = ASSET_DIR / "assets/mani_skill2_ycb/models" / id
+
     if add_collision:
         collision_file = str(model_dir / "collision.ply")
+        print(str(model_dir))
         builder.add_multiple_convex_collisions_from_file(
-            filename=collision_file,
-            scale=[scale] * 3,
-            material=physical_material,
-            density=density,
-        )
+          filename=collision_file,
+          decomposition="coacd",
+          decomposition_params={
+              "preprocess_mode": "box",          
+              "threshold": 0.05,
+              "preprocess_resolution": 50,       
+          },
+          scale=[scale] * 3,
+          material=physical_material,
+          density=density,
+      )
+
     if add_visual:
         visual_file = str(model_dir / "textured.obj")
         builder.add_visual_from_file(filename=visual_file, scale=[scale] * 3)
